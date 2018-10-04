63d441189ef69790df2fcbaa813b668793c845f7
scizorman <tueda1207@gmail.com>
2018-10-04 03:41:51 -0500

    <netcdf2qlook> changed organization of arguments

--- netcdf2qlook.py
+++ netcdf2qlook.py
@@ -13,6 +13,7 @@ SCIS_DIR = Path("../scis").expanduser()
 CALS_DIR = Path("../cals").expanduser()
 QLOOK_DIR = Path("../qlooks").expanduser()
 PLOT_TYPE = ["otfmap", "psw", "timestream"]
+IF_NUM = 4
 IF_LABELS = [
     "IF1: B-POL (LSB)",
     "IF2: B-POL (USB)",
@@ -24,59 +25,95 @@ AXIS_LABEL_DICT = {
     "psw": {"x": "Frequency [GHz]", "y": "Power [K]"},
     "timestream": {"x": "Time stream", "y": "Power [a.u.]"},
 }
+COLORS = ["tomato", "olivedrab", "palevioletred", "steelblue"]
 T_AMB = 273.
 FREQ = (128 - 0.1) + np.linspace(0., 2.5, 2**15)
 
 
 class NetCDF2Qlook(object):
+    """Create quick-look plot from NetCDF file
+
+    Args:
+        obs_id (str): ID of the observational data.
+        plot_type (str): Plot type.
+            Allowed values are 'otfmap', 'psw', or 'timestream'.
+            Defaults to 'timestream'
+        cal_id (str): ID of the calibration data.
+            If you plot 'otfmap' or 'psw', you need this ID.
+            Defaults to None.
+        title (str): Title of quick-look plot.
+            Defaults to 'Q-Look Plot'.
+
+    Raises:
+        FileNotFoundError: If doesn't exists files corresponded to ID
+        TypeError: If the plot type is invalid
     """
-    """
-    def __init__(self, sci_id, ptype, cal_id=None, title="Q-Look Plot"):
+    def __init__(self, obs_id, plot_type, cal_id=None, title="Q-Look Plot"):
         self.scis = [
-            SCIS_DIR / f"xffts{sci_id}.xfftsx.0{i}.nc" for i in [1, 2, 3, 4]
+            SCIS_DIR / f"xffts{obs_id}.xfftsx.0{i}.nc"
+            for i in [1, 2, 3, 4]
         ]
         if not all([f.exists() for f in self.scis]):
-            raise AssertionError
+            raise FileNotFoundError(f"Data (ID : {obs_id}) : not found")
+
+        if not plot_type in PLOT_TYPE:
+            raise TypeError(f"{plot_type} : invalid type of plot")
+        self.type = plot_type
 
         if cal_id is not None:
             self.cals = [
-                CALS_DIR / f"xffts{cal_id}.xfftsx.0{i}.nc" for i in [1, 2, 3, 4]
+                CALS_DIR / f"xffts{cal_id}.xfftsx.0{i}.nc"
+                for i in [1, 2, 3, 4]
             ]
             if not all([f.exists() for f in self.cals]):
-                raise AssertionError
+                raise FileNotFoundError(
+                    f"Calibration data (ID : {cal_id}) : not found"
+                )
 
-        if not ptype in PLOT_TYPE:
-            raise AssertionError
-        self.ptype = ptype
+        self.qlook = QLOOK_DIR / f"{obs_id}_{plot_type}.html"
 
-        self.sci_id = sci_id
+        self.obs_id = obs_id
         self.cal_id = cal_id
         self.title = title
-        self.qlook = QLOOK_DIR / f"{sci_id}_{ptype}.html"
 
     def otfmap(self):
+        """Plot the OTF map
+
+        Args:
+            None
+
+        Return:
+            plots (bokeh.models.layouts.Column): Plot of OTF map
         """
-        """
-        print("unimplemented...")
+        print("not implemented...")
         sys.exit()
 
     def psw(self):
-        """
+        """Plot the PSW data
+
+        Args:
+            None
+
+        Return:
+            plots (bokeh.models.layouts.Column): Plot of PSW data
         """
         Rs, ONs, OFFs = [], [], []
         for s, c in zip(self.scis, self.cals):
             with xr.open_dataset(s) as sci, xr.open_dataset(c) as cal:
-                Rs.append((cal["array"]/cal["integtime"]).mean("t"))
-                ONs.append((sci["array"]/sci["integtime"])[sci["bufpos"]=="ON"])
-                OFFs.append((sci["array"]/sci["integtime"])[sci["bufpos"]=="REF"])
+                Rs.append((cal["array"] / cal["integtime"]).mean("t"))
+                ONs.append(
+                    (sci["array"] / sci["integtime"])[sci["bufpos"]=="ON"]
+                )
+                OFFs.append(
+                    (sci["array"] / sci["integtime"])[sci["bufpos"]=="REF"]
+                )
         Tcals = [
             T_AMB * (on - off[1:]) / (r - off[1:])
             for r, on, off in zip(Rs, ONs, OFFs)
         ]
 
         plots = []
-        colors = ["tomato", "olivedrab", "palevioletred", "steelblue"]
-        for i, (t, c) in enumerate(zip(Tcals, colors)):
+        for i, (t, c) in enumerate(zip(Tcals, COLORS)):
             if i == 0:
                 p = figure(title=self.title, plot_width=640, plot_height=360)
             else:
@@ -89,93 +126,95 @@ class NetCDF2Qlook(object):
         return plots
 
     def timestream(self):
-        """
+        """Plot the time stream data
+
+        Args:
+            None
+
+        Return:
+            plots (bokeh.models.layouts.Column): Plot of time stream data
         """
         plots = []
-        colors = ["tomato", "olivedrab", "palevioletred", "steelblue"]
-        for i, (s, c) in enumerate(zip(self.scis, colors)):
+        for i, (s, c) in enumerate(zip(self.scis, COLORS)):
             with xr.open_dataset(s) as data:
-                _arrays = data["array"]
-                _integ_data = np.average(_arrays, axis=1)
-                # _integ_data = _arrays[:, 8000:24000].mean("array_dim0").values
-                _x = np.arange(len(_integ_data))
+                arrays = data["array"]
+                integ_data = np.average(arrays, axis=1)
+                x = np.arange(len(integ_data))
 
             if i == 0:
                 p = figure(title=self.title, plot_width=640, plot_height=360)
             else:
                 p = figure(plot_width=640, plot_height=360)
-            p.line(_x, _integ_data,
-                   color=c, line_width=1., legend=IF_LABELS[i])
+            p.line(x, integ_data, color=c,
+                   line_width=1., legend=IF_LABELS[i])
             plots.append(self._create_looks(p))
 
         plots = gridplot([plots[0], plots[1]], [plots[2], plots[3]])
         return plots
 
-        # with xr.open_dataset(self.scis[1]) as data:
-        #     _arrays = data["array"]
-        #     _integ_data = np.average(_arrays, axis=1)
-        #     _x = np.arange(len(_integ_data))
-        #
-        # p = figure(title=self.title, plot_width=960, plot_height=540)
-        # p.line(_x, _integ_data, legend="Timestream",
-        #        line_color="midnightblue", line_width=1.)
-        # return self._create_looks(p)
-        #
     def save(self):
-        """
+        """Save the plot
+
+        Args:
+            None
+
+        Return:
+            None
         """
         output_file(self.qlook)
-        if self.ptype == "otfmap":
+        if self.type == "otfmap":
             p = self.otfmap()
-        elif self.ptype == "psw":
+        elif self.type == "psw":
             p = self.psw()
-        elif self.ptype == "timestream":
+        elif self.type == "timestream":
             p = self.timestream()
         else:
-            raise AssertionError
+            raise TypeError(f"{plot_type} : invalid type of plot")
         save(p)
+        return
 
     def _create_looks(self, plot, power_unit="a.u."):
-        """whwh
+        """Create the looks of the plot
+
+        Args:
+            plot (bokeh.plotting.figure.Figure): Instance of the plot
+
+        Return:
+            plot (bokeh.plotting.figure.Figure): Instance of the shaped plot
         """
         plot.title.text_font_size = "16pt"
         plot.legend.location = "top_right"
         plot.legend.background_fill_alpha = 0.5
-        plot.xaxis.axis_label = AXIS_LABEL_DICT[self.ptype]["x"]
+        plot.xaxis.axis_label = AXIS_LABEL_DICT[self.type]["x"]
         plot.xaxis.axis_label_text_font_size = "12pt"
         plot.xaxis.axis_label_text_font_style = "bold"
-        plot.yaxis.axis_label = AXIS_LABEL_DICT[self.ptype]["y"]
+        plot.yaxis.axis_label = AXIS_LABEL_DICT[self.type]["y"]
         plot.yaxis.axis_label_text_font_size = "12pt"
         plot.yaxis.axis_label_text_font_style = "bold"
         return plot
 
 
 def main():
-    """
-    """
-    usage = "python netcdf2qlook.py SCI, CAL PTYPE [--title <title>] [--help]"
+    _usage = "aaa"
     parser = argparse.ArgumentParser(
         prog="netcdf2qlook",
-        usage=usage,
-        description="create Q-look plot (Bokeh html file)",
+        usage=_usage,
+        description="create Q-Look plot (Bokeh's html file)",
         add_help=True,
     )
-    parser.add_argument("sci", help="ID of the science data")
-    parser.add_argument("ptype", type=str, default="timestream",
-                        choices=["otfmap", "psw", "timestream"],
+    parser.add_argument("obs_id", type=str,
+                        help="ID of the observational data")
+    parser.add_argument("-t", "--type", type=str, default="timestream",
+                        dest="type", choices=["otfmap", "psw", "timestream"],
                         help="type of plot ('otfmap', 'psw', 'timestream')")
-    parser.add_argument("-c", "--calibration", type=str,
-                        dest="cal", help="ID of the calibration data")
-    parser.add_argument("-t", "--title", type=str,
-                        dest="title", help="title of plot")
+    parser.add_argument("-c", "--cal_id", type=str,
+                        dest="cal_id", help="ID of the calibration data")
+    parser.add_argument("--title", type=str, dest="title",
+                        default="Q-Look plot", help="title of plot")
     args = parser.parse_args()
 
-    if args.title:
-        n2q = NetCDF2Qlook(args.sci, args.ptype,
-                           cal_id=args.cal, title=args.title)
-    else:
-        n2q = NetCDF2Qlook(args.sci, args.ptype, cal_id=args.cal)
-
+    n2q = NetCDF2Qlook(args.obs_id, args.type,
+                       cal_id=args.cal_id, title=args.title)
     n2q.save()
     return
 
