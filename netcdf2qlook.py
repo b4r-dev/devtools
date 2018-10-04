f230431f72dfba3377efabec883c5318846fd69a
scizorman <tueda1207@gmail.com>
2018-10-03 21:47:42 -0500

    <netcdf2qlook> enable psw & timestream

--- netcdf2qlook.py
+++ netcdf2qlook.py
@@ -1,68 +1,78 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import sys
 import argparse
-import re
-from pathlib import Path
-
 import numpy as np
 import xarray as xr
-from bokeh.io import output_file, save
+from pathlib import Path
+from bokeh.plotting import figure, output_file, save
 from bokeh.layouts import gridplot
-from bokeh.plotting import figure
 
 
 SCIS_DIR = Path("../scis").expanduser()
 CALS_DIR = Path("../cals").expanduser()
 QLOOK_DIR = Path("../qlooks").expanduser()
-IF_LABEL_DICT = {
-    1: "B-Pol LSB (IF 1)",
-    2: "B-Pol USB (IF 2)",
-    3: "A-Pol LSB (IF 3)",
-    4: "A-Pol USB (IF 4)",
+PLOT_TYPE = ["otfmap", "psw", "timestream"]
+IF_LABELS = [
+    "IF1: B-POL (LSB)",
+    "IF2: B-POL (USB)",
+    "IF3: A-POL (LSB)",
+    "IF4: A-POL (USB)",
+]
+AXIS_LABEL_DICT = {
+    "otfmap": {"x": "unknow", "y": "unknown"},
+    "psw": {"x": "Frequency [GHz]", "y": "Power [K]"},
+    "timestream": {"x": "Frequency [GHz]", "y": "Power [a.u.]"},
 }
+T_AMB = 273.
+FREQ = (128 - 0.1) + np.linspace(0., 2.5, 2**15)
 
 
 class NetCDF2Qlook(object):
     """
     """
-    def __init__(self, sci_id, cal_id, title="Q-Look Plot"):
-        self.scis = [SCIS_DIR / f"xffts{sci_id}.xfftsx.0{i}.nc"
-                    for i in range(1, 5)]
-        if not all([sci.exists for sci in self.scis]):
-            return AssertionError
+    def __init__(self, sci_id, ptype, cal_id=None, title="Q-Look Plot"):
+        self.scis = [
+            SCIS_DIR / f"xffts{sci_id}.xfftsx.0{i}.nc" for i in [1, 2, 3, 4]
+        ]
+        if not all([f.exists() for f in self.scis]):
+            raise AssertionError
 
-        self.cals = [CALS_DIR / f"xffts{cal_id}.xfftsx.0{i}.nc"
-                     for i in range(1, 5)]
-        if not all([cal.exists for cal in self.cals]):
-            return AssertionError
+        if cal_id is not None:
+            self.cals = [
+                CALS_DIR / f"xffts{cal_id}.xfftsx.0{i}.nc" for i in [1, 2, 3, 4]
+            ]
+            if not all([f.exists() for f in self.cals]):
+                raise AssertionError
+
+        if not ptype in PLOT_TYPE:
+            raise AssertionError
+        self.ptype = ptype
 
         self.sci_id = sci_id
         self.cal_id = cal_id
         self.title = title
+        self.qlook = QLOOK_DIR / f"{sci_id}_{ptype}.html"
 
-    def map(self):
+    def otfmap(self):
         """
         """
-        pass
+        print("unimplemented...")
+        sys.exit()
 
     def psw(self):
         """
         """
-        Tamb = 273.
-        freq = (128 - 0.1) + np.linspace(0., 2.5, 2**15)
-
-        Rs = []
-        ONs = []
-        OFFs = []
+        Rs, ONs, OFFs = [], [], []
         for s, c in zip(self.scis, self.cals):
-            with xr.open_dataset(s) as sci, \
-                    xr.open_dataset(c) as cal:
+            with xr.open_dataset(s) as sci, xr.open_dataset(c) as cal:
                 Rs.append((cal["array"]/cal["integtime"]).mean("t"))
                 ONs.append((sci["array"]/sci["integtime"])[sci["bufpos"]=="ON"])
                 OFFs.append((sci["array"]/sci["integtime"])[sci["bufpos"]=="REF"])
-        Tcals = [Tamb * (on - off[1:]) / (r - off[1:])
-                 for r, on, off in zip(Rs, ONs, OFFs)]
-        # Tcals = [t - t.mean("array_dim0") for t in Tcals]
+        Tcals = [
+            T_AMB * (on - off[1:]) / (r - off[1:])
+            for r, on, off in zip(Rs, ONs, OFFs)
+        ]
 
         plots = []
         colors = ["tomato", "olivedrab", "palevioletred", "steelblue"]
@@ -71,24 +81,12 @@ class NetCDF2Qlook(object):
                 p = figure(title=self.title, plot_width=640, plot_height=360)
             else:
                 p = figure(plot_width=640, plot_height=360)
-            p.line(freq, t.mean("t").values,
-                   color=c, line_width=1., legend=IF_LABEL_DICT[i+1])
-
-            p.title.text_font_size = "16pt"
-            p.legend.location = "top_right"
-            p.legend.background_fill_alpha = 0.5
-            p.xaxis.axis_label = "Frequency [GHz]"
-            p.xaxis.axis_label_text_font_size = "12pt"
-            p.xaxis.axis_label_text_font_style = "bold"
-            p.yaxis.axis_label = "Power [K]"
-            p.yaxis.axis_label_text_font_size = "12pt"
-            p.yaxis.axis_label_text_font_style = "bold"
-
-            plots.append(p)
+            p.line(FREQ, t.mean("t").values,
+                   color=c, line_width=1., legend=IF_LABELS[i])
+            plots.append(self._create_looks(p))
 
         plots = gridplot([plots[0], plots[1]], [plots[2], plots[3]])
         return plots
-
 
     def timestream(self):
         """
@@ -99,57 +97,66 @@ class NetCDF2Qlook(object):
             _x = np.arange(len(_integ_data))
 
         p = figure(title=self.title, plot_width=960, plot_height=540)
-        p.line(_x, _integ_data, line_color="midnightblue", line_width=1.)
-        p.title.text_font_size = "16pt"
-        p.xaxis.axis_label = "Time stream"
-        p.xaxis.axis_label_text_font_size = "12pt"
-        p.xaxis.axis_label_text_font_style = "bold"
-        p.yaxis.axis_label = "Power [a.u.]"
-        p.yaxis.axis_label_text_font_size = "12pt"
-        p.yaxis.axis_label_text_font_style = "bold"
+        p.line(_x, _integ_data, legend="Timestream",
+               line_color="midnightblue", line_width=1.)
+        return self._create_looks(p)
 
-        return p
-    
-
-    def save(self, plot):
+    def save(self):
         """
         """
-        _qlook = QLOOK_DIR / f"{self.sci_id}.html"
-        output_file(_qlook)
-        save(plot)
+        output_file(self.qlook)
+        if self.ptype == "otfmap":
+            p = self.otfmap()
+        elif self.ptype == "psw":
+            p = self.psw()
+        elif self.ptype == "timestream":
+            p = self.timestream()
+        else:
+            raise AssertionError
+        save(p)
+
+    def _create_looks(self, plot, power_unit="a.u."):
+        """whwh
+        """
+        plot.title.text_font_size = "16pt"
+        plot.legend.location = "top_right"
+        plot.legend.background_fill_alpha = 0.5
+        plot.xaxis.axis_label = AXIS_LABEL_DICT[self.ptype]["x"]
+        plot.xaxis.axis_label_text_font_size = "12pt"
+        plot.xaxis.axis_label_text_font_style = "bold"
+        plot.yaxis.axis_label = AXIS_LABEL_DICT[self.ptype]["y"]
+        plot.yaxis.axis_label_text_font_size = "12pt"
+        plot.yaxis.axis_label_text_font_style = "bold"
+        return plot
 
 
 def main():
     """
     """
+    usage = "python netcdf2qlook.py SCI, CAL PTYPE [--title <title>] [--help]"
     parser = argparse.ArgumentParser(
         prog="netcdf2qlook",
-        usage="Create Q-look plot ('Bokeh' html file)",
-        description="description",
+        usage=usage,
+        description="create Q-look plot (Bokeh html file)",
         add_help=True,
     )
-    parser.add_argument("sci_id", help="date of the data")
-    parser.add_argument("cal_id", help="date of the calibration data")
-    parser.add_argument("plot", type=str, choices=["map", "psw", "timestream"],
-                        help="a kind of plot")
-    parser.add_argument("-t", "--title", type=str, dest="title", help="title of plot") 
+    parser.add_argument("sci", help="ID of the science data")
+    parser.add_argument("ptype", type=str, default="timestream",
+                        choices=["otfmap", "psw", "timestream"],
+                        help="type of plot ('otfmap', 'psw', 'timestream')")
+    parser.add_argument("-c", "--calibration", type=str,
+                        dest="cal", help="ID of the calibration data")
+    parser.add_argument("-t", "--title", type=str,
+                        dest="title", help="title of plot")
     args = parser.parse_args()
 
-    if args.title is not False:
-        n2q = NetCDF2Qlook(args.sci_id, args.cal_id, args.title)
+    if args.title:
+        n2q = NetCDF2Qlook(args.sci, args.ptype,
+                           cal_id=args.cal, title=args.title)
     else:
-        n2q = NetCDF2Qlook(args.sci_id, args.cal_id)
+        n2q = NetCDF2Qlook(args.sci, args.ptype, cal_id=args.cal)
 
-    if args.plot == "map":
-        plot = n2q.map()
-    elif args.plot == "psw":
-        plot = n2q.psw()
-    elif args.plot == "timestream":
-        plot = n2q.timestream()
-    else:
-        raise AssertionError
-    n2q.save(plot)
-
+    n2q.save()
     return
 
 
